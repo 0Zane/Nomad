e81d18be4f100bd56962a58c78791ffca83090ab
Zane <labs.zane@gmail.com>
2026-04-24 20:47:08 +0200

    Including headers + reading serial commands

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 
-#include "features.h"
 
-float readtemp(int);
+#include "readtemperature.h"
+#include "flashlight.h"
+
+float readtemp();
 void flashlight(bool);
 
 
@@ -10,16 +12,41 @@ void setup(){
 
     Serial.begin(9600);
     pinMode(1, OUTPUT);
-
+    pinMode(7, OUTPUT);
+    pinMode(15, OUTPUT); // RED LED
 }
 
 
 void loop(){
-  
-    Serial.println(readtemp());
-    flashlight(true);
-    delay(1000);
-    flashlight(false);
-    delay(1000);
 
-}
+    while (Serial.available() ==0 ){
+        if(digitalRead(15) == 0){
+            digitalWrite(7,0);
+            digitalWrite(15,1);
+        }
+    }
+        String command = Serial.readStringUntil('\n');
+        digitalWrite(15,0);
+        if(digitalRead(7) == 0){
+
+            digitalWrite(7,1);
+        }
+
+
+            if (command == "o"){
+                Serial.println("Flashlight_on");
+                flashlight(1); 
+            }else if (command == "f"){
+
+                flashlight(0);
+                Serial.println("Flashlight_off");
+            } else if (command == "r"){
+
+                Serial.println(readtemp());
+            } else { 
+                Serial.println("Unknown command");
+                Serial.print(command);
+            }
+
+
+    }
