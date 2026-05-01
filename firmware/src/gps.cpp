7ee685121dcec8eac67d5af12d68e49fc45a76af
Zane <labs.zane@gmail.com>
2026-05-01 23:32:29 +0200

    Waiting for GPS data buffer to empty before outputting in a while loop,

--- firmware/src/gps.cpp
+++ firmware/src/gps.cpp
@@ -7,31 +7,27 @@ TinyGPSPlus gps;
 HardwareSerial gpsSerial(2);
 
 
-
-
 void readgps() {
+  while (gpsSerial.available() > 0) {
+    gps.encode(gpsSerial.read());
+  }
 
-    if (gpsSerial.available() > 0) {
-
-      gps.encode(gpsSerial.read());
-    
-    }
-    if (gps.location.isUpdated()) {
-      Serial.print("LAT: ");
-      Serial.println(gps.location.lat(), 6);
-      Serial.print("LONG: "); 
-      Serial.println(gps.location.lng(), 6);
-      Serial.print("SPEED (km/h) = "); 
-      Serial.println(gps.speed.kmph()); 
-      Serial.print("ALT (min)= "); 
-      Serial.println(gps.altitude.meters());
-      Serial.print("HDOP = "); 
-      Serial.println(gps.hdop.value() / 100.0); 
-      Serial.print("Satellites = "); 
-      Serial.println(gps.satellites.value()); 
-      Serial.print("Time in UTC: ");
-      Serial.println(String(gps.date.year()) + "/" + String(gps.date.month()) + "/" + String(gps.date.day()) + "," + String(gps.time.hour()) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second()));
-      Serial.println("");
-    }
+  if (gps.location.isUpdated()) {
+    Serial.print("LAT: ");
+    Serial.println(gps.location.lat(), 6);
+    Serial.print("LONG: "); 
+    Serial.println(gps.location.lng(), 6);
+    Serial.print("SPEED (km/h) = "); 
+    Serial.println(gps.speed.kmph()); 
+    Serial.print("ALT (min)= "); 
+    Serial.println(gps.altitude.meters());
+    Serial.print("HDOP = "); 
+    Serial.println(gps.hdop.value() / 100.0); 
+    Serial.print("Satellites = "); 
+    Serial.println(gps.satellites.value()); 
+    Serial.print("Time in UTC: ");
+    Serial.println(String(gps.date.year()) + "/" + String(gps.date.month()) + "/" + String(gps.date.day()) + "," + String(gps.time.hour()) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second()));
+    Serial.println("");
+  }
 
 }
