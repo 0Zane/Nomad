39309e4c1e0bd6c1933f30b15cb838495716cf15
Zane <labs.zane@gmail.com>
2026-05-11 22:51:55 +0200

    Adding prototyping LoRa pins

--- firmware/include/pins.h
+++ firmware/include/pins.h
@@ -1,4 +1,4 @@
-//I2C BUS
+//I2C BUS (Temperature sensor & GPIO extender)
 #define TEMP_SCL 9
 #define TEMP_SDA 8
 
@@ -14,3 +14,10 @@
 
 //FLASHLIGHT
 #define FLASHLIGHT 1
+
+//lORA MODULE
+#define AUX 20
+#define TX3 21
+#define RX3 47
+#define MI 48
+#define MO 45
