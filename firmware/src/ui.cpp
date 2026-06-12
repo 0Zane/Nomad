1adf53b366478a819648a4cbb7707674dc64963c
Zane <labs.zane@gmail.com>
2026-06-12 23:58:56 +0200

    Updating UI code

--- firmware/src/ui.cpp
+++ firmware/src/ui.cpp
@@ -2,26 +2,29 @@
 #include <math.h>
 #include "ui.h"
 #include <TFT_eSPI.h>
-#include "nomadlogo.h"
+#include "icon.h"
 
 TFT_eSPI tft = TFT_eSPI();
 
 void drawboot(){
     Serial.println("Drawing booting screen...");
-    tft.fillScreen(TFT_BLUE);
-    // Try to auto-detect a square image size from the logo data
-    size_t len = sizeof(logo) / sizeof(logo[0]);
-    int imgW = 200;
-    int imgH = 200;
-    if (len > 0) {
-        int side = (int)sqrt((double)len + 0.5);
-        if ((size_t)(side * side) == len) {
-            imgW = imgH = side;
-        }
-    }
-    int x = (tft.width() - imgW) / 2;
-    int y = (tft.height() - imgH) / 2;
+    tft.fillScreen(TFT_BLACK);
+    int x = (tft.width() - 300) / 2;
+    int y = (tft.height() - 144) / 2;
     tft.setSwapBytes(true);
-    tft.pushImage(x, y, imgW, imgH, logo);
+    tft.pushImage(x, y, 300, 144, logo);
     Serial.println("BLUE SCREEN HAS BEEN INITIALIZED");
 }
+
+
+void drawenv(){}
+
+void drawflashlight(){}
+
+void drawgps(){}
+
+void drawlora(){}
+
+void drawtips(){}
+
+void drawdevice(){}
